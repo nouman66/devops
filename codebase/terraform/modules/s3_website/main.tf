601fd91b770639066986468921069a5f96a7592e
Nouman Butt <nouman.butt@tune.com>
2024-02-12 04:35:24 +0500

    test

--- codebase/terraform/modules/s3_website/main.tf
+++ codebase/terraform/modules/s3_website/main.tf
@@ -2,15 +2,6 @@ resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_public_access_block" "public_access_block" {
-  bucket = aws_s3_bucket.bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.bucket.id
 
