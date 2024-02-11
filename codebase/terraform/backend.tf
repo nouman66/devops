2563e89315d22a1b0f9e67884bb86cf8de360121
Nouman Butt <nouman.butt@tune.com>
2024-02-12 02:25:56 +0500

    test

--- codebase/terraform/backend.tf
+++ codebase/terraform/backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket-challenge"
-    key            = "state/${terraform.workspace}/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
