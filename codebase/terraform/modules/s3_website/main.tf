c671379a435396e433bfed2442cdb443baf1660a
Nouman Butt <nouman.butt@tune.com>
2024-02-12 03:43:01 +0500

    test

--- codebase/terraform/modules/s3_website/main.tf
+++ codebase/terraform/modules/s3_website/main.tf
@@ -2,11 +2,6 @@ resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
   acl    = "public-read"
 
-  website {
-    index_document = var.index_document
-    error_document = var.error_document
-  }
-
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -20,10 +15,23 @@ resource "aws_s3_bucket" "bucket" {
   })
 }
 
-resource "aws_s3_bucket_object" "website_files" {
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.bucket.id
+
+  index_document {
+    suffix = var.index_document
+  }
+
+  error_document {
+    key = var.error_document
+  }
+}
+
+resource "aws_s3_object" "website_files" {
   for_each = fileset(var.website_content_directory, "**/*")
   bucket   = aws_s3_bucket.bucket.id
   key      = each.value
   source   = "${var.website_content_directory}/${each.value}"
   etag     = filemd5("${var.website_content_directory}/${each.value}")
 }
+
