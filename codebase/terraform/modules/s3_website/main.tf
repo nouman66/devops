978e8437777fb63e58a601d3e32fa05b36b72761
Nouman Butt <nouman.butt@tune.com>
2024-02-12 04:43:46 +0500

    test

--- codebase/terraform/modules/s3_website/main.tf
+++ codebase/terraform/modules/s3_website/main.tf
@@ -2,6 +2,13 @@ resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 }
 
+resource "aws_s3_bucket_ownership_controls" "controls" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "public_access_block" {
   bucket = aws_s3_bucket.bucket.id
 
@@ -9,6 +16,16 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.controls,
+    aws_s3_bucket_public_access_block.public_access_block,
+  ]
+
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
