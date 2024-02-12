d1d254a3445deff566924c0c6d235231d49829c3
Nouman Butt <nouman.butt@tune.com>
2024-02-12 08:18:01 +0500

    test

--- codebase/terraform/main.tf
+++ codebase/terraform/main.tf
@@ -5,6 +5,6 @@ provider "aws" {
 module "s3_website" {
   source = "./modules/s3_website"
 
-  bucket_name              = "${terraform.workspace}-mywebsite"
-  website_content_directory = "../path/to/your/website/build"
+  bucket_name              = "${terraform.workspace}-website"
+
 }
