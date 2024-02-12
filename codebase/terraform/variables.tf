b79a8e7c6748869956ec54bff3bd5ff51ab2274b
Nouman Butt <nouman.butt@tune.com>
2024-02-12 08:23:55 +0500

    test

--- codebase/terraform/variables.tf
+++ codebase/terraform/variables.tf
@@ -2,8 +2,3 @@ variable "aws_region" {
   type        = string
   description = "aws region"
 }
-
-variable "website_content_directory" {
-  type        = string
-  description = "Local directory containing website content"
-}
